277ae02b7eb5938a4b2d706a632261ce80bb725e
김근홍 <xerlock.203@gmail.com>
2026-08-11 16:48:29 +0900

    feat: Hands on Composition

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup>
+import WeatherComposition from './components/Weather/WeatherComposition.vue'
 import WeatherMockup from './components/Weather/WeatherMockup.vue'
 </script>
 
 <template>
   <div class="app-container">
+    <h1>Weather Mockup</h1>
     <WeatherMockup />
+  </div>
+
+  <div class="app-container">
+    <h1>Weather Composition</h1>
+    <WeatherComposition />
   </div>
 </template>
 
